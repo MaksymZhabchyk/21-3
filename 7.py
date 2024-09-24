04d158c0160a27016e697b078a61f2334825e808
MaksymZhabchyk <zabcikmaksim@gmail.com>
2024-09-24 14:01:59 +0300

    Add files via upload

---
+++ 7.py
@@ -0,0 +1,14 @@
+my_info = {
+    "name": "Maksym",
+    "age": 16,
+    "details": {
+        "hobby": "judo",
+        "favorite_language": "Ukraine-English",
+        "experience_years": 1,
+        "city": "Lutsk",
+        "sport": "jiujitsu"
+    },
+    "profession": "computer engineer"
+}
+
+print(my_info)
